5ef4e560503291ea57f08d17801f57226be3254b
usmanianas <anasusmani2010@gmail.com>
2021-02-22 16:17:05 +0530

    scipy bugs fixed

--- nonuniform.py
+++ nonuniform.py
@@ -1,26 +1,27 @@
 import numpy as np
 import pylab as plt
-from scipy import integrate
+import scipy.integrate as integrate
 
 Nx = 100
 
 xi = np.linspace(0, 1, Nx, endpoint=True)
 
+fxi = np.exp(xi**2) 
+#fxi = np.random.random_sample(Nx)
+
+
 beta = 1.0e0
 
 x = 0.5*(1.0 - np.tanh(beta*(1.0 - 2*xi))/np.tanh(beta))
 
-#fxi = xi**2 #np.random.random_sample(Nx)
-fxi = np.random.random_sample(Nx)
-
-
 dx_dxi = beta*(1.0/np.cosh(beta*(1.0-2*xi)))**2/np.tanh(beta)
+
 dxi_dx = 1.0/dx_dxi
 
-Axi = integrate.simpson(fxi, xi)
+Axi = integrate.simps(fxi, xi)
 print("Axi = ", Axi)
 
-Ax = integrate.simpson(fxi*dxi_dx, x)
+Ax = integrate.simps(fxi*dxi_dx, x)
 print("Ax = ", Ax)
 
 print("%error = ", ((Axi-Ax)/Ax)*100)
